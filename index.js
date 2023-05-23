cc1b352bf44d010246f1bda99e121b7ae125299a
Oleg <Oleg@DESKTOP-B5TOANR>
2023-05-23 17:47:27 +0300

    Other operations implemented

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const yargs = require("yargs");
-const { hideBin } = require('yargs/helpers')
+const {hideBin} = require('yargs/helpers')
 const {Tasks} = require("./tasks")
 
 yargs(hideBin(process.argv))
@@ -11,6 +11,32 @@ yargs(hideBin(process.argv))
     }, async (argv) => {
         const data = await new Tasks().createTask(argv.task)
         console.log(data.rows[0]);
+    })
+    .command('get', 'Get all tasks', (yargs) => {
+        return yargs
+    }, async (argv) => {
+        const data = await new Tasks().getAllTasks()
+        console.log(data.rows);
+    })
+    .command('update [id] [task]', 'Update task text', (yargs) => {
+        return yargs
+            .positional('id', {
+                describe: 'Task id',
+            }).positional('task', {
+                describe: 'New task text',
+            })
+    }, async (argv) => {
+        const data = await new Tasks().updateTask(argv.id, argv.task)
+        console.log(data.rows[0]);
+    })
+    .command('delete [id]', 'Delete task', (yargs) => {
+        return yargs
+            .positional('id', {
+                describe: 'Task id',
+            })
+    }, async (argv) => {
+        await new Tasks().deleteTask(argv.id);
+        console.log(`Task with id ${argv.id} deleted!`);
     })
     .parse()
 
--- tasks.js
+++ tasks.js
@@ -6,6 +6,18 @@ class Tasks {
     async createTask(name){
         return Tasks.Database.query('INSERT INTO tasks (task_message, task_time) VALUES($1, $2) RETURNING *', [name, new Date()]);
     }
+
+    async getAllTasks(){
+        return Tasks.Database.query('SELECT * FROM tasks');
+    }
+
+    async updateTask(id, name){
+        return Tasks.Database.query('UPDATE tasks SET task_message = $1, task_time = $2 WHERE task_id = $3 RETURNING *', [name, new Date(), id]);
+    }
+
+    async deleteTask(id){
+        return Tasks.Database.query('DELETE FROM tasks WHERE task_id = $1', [id]);
+    }
 }
 
 module.exports.Tasks = Tasks
